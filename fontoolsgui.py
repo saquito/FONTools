42c37525b009ac2b71e4b80f0cf4a30cd3c995c4
msaco <martino.saco@gtd.eu>
2018-10-18 18:01:17 +0200

    starting expansion panel

--- fontoolsgui.py
+++ fontoolsgui.py
@@ -34,6 +34,7 @@ class FonToolsGUI(tk.Toplevel):
       self.configuration_frame.updatedb_btn.pack()
       self.configuration_frame.pack(expand=True, fill='both',side=LEFT)
       self.overview_frame = OverviewPanedWindow(pageOverview)
+      self.expansions_frame = ExpansionsPanedWindow(pageExpansions)
 
       self.conn = bgs.get_db_connection()
       
@@ -71,35 +72,41 @@ class OverviewPanedWindow(tk.PanedWindow):
         self.combo = ttk.Combobox(self)
         self.combo.pack(side=TOP)
         self.combo.bind("<<ComboboxSelected>>", self.overview_selection_changed)
-        self.treeview = ttk.Treeview(self,columns=("Faction","Influence","State","Pending","Recovering","Player","Last Update"))
-        self.treeview.heading("#0",text="#")
-        self.treeview.column("#0",width=30,stretch=NO,anchor=CENTER)
-        self.treeview.heading("Faction",text="Faction",anchor=CENTER)
-        self.treeview.column("Faction",width=240,stretch=NO,anchor="w")
-        self.treeview.heading("Influence",text="Influence",anchor=CENTER)
-        self.treeview.column("Influence",width=50,stretch=NO,anchor=CENTER)
-        self.treeview.heading("State",text="State",anchor=CENTER)
-        self.treeview.column("State",width=150,stretch=NO,anchor=CENTER)
-        self.treeview.heading("Pending",text="Pending",anchor=CENTER)
-        self.treeview.column("Pending",width=240,stretch=NO,anchor=CENTER)
-        self.treeview.heading("Recovering",text="Recovering",anchor=CENTER)
-        self.treeview.column("Recovering",width=240,stretch=NO,anchor=CENTER)
-        self.treeview.heading("Player",text="Player",anchor=CENTER)
-        self.treeview.column("Player",width=140,stretch=NO,anchor=CENTER)
-        self.treeview.heading("Last Update",text="Last Update",anchor=CENTER)
-        self.treeview.column("Last Update",width=140,stretch=NO,anchor=CENTER)
-        self.treeview.pack(expand=True, fill='both')
-        self.treeview.bind('<<TreeviewSelect>>',self.selectItemCallback)
+        self.factions_tree = ttk.Treeview(self,columns=("Faction","Influence","State","Pending","Recovering","Player","Last Update"))
+        self.factions_tree.heading("#0",text="#")
+        self.factions_tree.column("#0",width=30,stretch=NO,anchor=CENTER)
+        self.factions_tree.heading("Faction",text="Faction",anchor=CENTER)
+        self.factions_tree.column("Faction",width=240,stretch=NO,anchor="w")
+        self.factions_tree.heading("Influence",text="Influence",anchor=CENTER)
+        self.factions_tree.column("Influence",width=50,stretch=NO,anchor=CENTER)
+        self.factions_tree.heading("State",text="State",anchor=CENTER)
+        self.factions_tree.column("State",width=150,stretch=NO,anchor=CENTER)
+        self.factions_tree.heading("Pending",text="Pending",anchor=CENTER)
+        self.factions_tree.column("Pending",width=240,stretch=NO,anchor=CENTER)
+        self.factions_tree.heading("Recovering",text="Recovering",anchor=CENTER)
+        self.factions_tree.column("Recovering",width=240,stretch=NO,anchor=CENTER)
+        self.factions_tree.heading("Player",text="Player",anchor=CENTER)
+        self.factions_tree.column("Player",width=140,stretch=NO,anchor=CENTER)
+        self.factions_tree.heading("Last Update",text="Last Update",anchor=CENTER)
+        self.factions_tree.column("Last Update",width=140,stretch=NO,anchor=CENTER)
+        self.factions_tree.pack(expand=True, fill='both')
+        self.factions_tree.bind('<<TreeviewSelect>>',self.selectItemCallback)
         self.pack(expand=True, fill='both',side=RIGHT)
-        self.combo["values"] = ["Naunin", "HR 6177", "Ratemere", "Maopi", "Smethells 1"]
-        self.conn = bgs.get_db_connection()
-    
+        self.combo["values"] = get_controlled_systems()
+        if self.combo["values"]:
+          self.combo.current(0)
+          print(self.combo.get())
+          self.update_overview(self.combo.get())
+        
+    def get_controlled_systems(self):
+      retun ["Naunin", "HR 6177", "Ratemere", "Maopi", "Smethells 1"]
+      
     def overview_selection_changed(self,event):
       system_name = self.combo.get()
       self.update_overview(system_name)
     
     def update_overview(self,system_name):
-      self.treeview.delete(*self.treeview.get_children())
+      self.factions_tree.delete(*self.factions_tree.get_children())
       i=0
       star_system = bgs.System(system_name)
       if star_system:
@@ -110,8 +117,90 @@ class OverviewPanedWindow(tk.PanedWindow):
           active_state = f.get_states('activeState')[0][2]
           pending_states = ", ".join([state[2] for state in f.get_states('pendingState')])
           recovering_states = ", ".join([state[2] for state in f.get_states('recoveringState')])
-          item = self.treeview.insert("", END, text=str(i), values=(f.name,influence*100.0,active_state,pending_states,recovering_states, "YES" if f.is_player else "NO",""))
+          item = self.factions_tree.insert("", END, text=str(i), values=(f.name,influence*100.0,active_state,pending_states,recovering_states, "YES" if f.is_player else "NO",""))
           i=i+1
+      
+    def selectItemCallback(self,event):
+      pass
+      
+class ExpansionsPanedWindow(tk.PanedWindow):
+    def __init__(self,master=None):
+        tk.PanedWindow.__init__(self,master)
+        
+        self.home_system_label = ttk.Label(self,text="Home system")
+        self.home_system_label.pack(side=LEFT)
+        self.combo = ttk.Combobox(self)
+        self.combo.pack(side=LEFT)
+        self.faction_label = ttk.Label(self,text="Faction")
+        self.faction_label.pack(side=BOTTOM)
+        self.combo.bind("<<ComboboxSelected>>", self.overview_selection_changed)
+        self.systems_tree = ttk.Treeview(self,columns=("System","Distance","Number of Factions","Has Player"))
+        self.systems_tree.heading("#0",text="#")
+        self.systems_tree.column("#0",width=30,stretch=NO,anchor=CENTER)
+        self.systems_tree.heading("System",text="System",anchor=CENTER)
+        self.systems_tree.column("System",width=240,stretch=NO,anchor="w")
+        self.systems_tree.heading("Distance",text="Distance",anchor=CENTER)
+        self.systems_tree.column("Distance",width=50,stretch=NO,anchor=CENTER)
+        self.systems_tree.heading("Number of Factions",text="Number of Factions",anchor=CENTER)
+        self.systems_tree.column("Number of Factions",width=150,stretch=NO,anchor=CENTER)
+        self.systems_tree.heading("Has Player",text="Has Player",anchor=CENTER)
+        self.systems_tree.column("Has Player",width=140,stretch=NO,anchor=CENTER)
+        self.systems_tree.pack(expand=True, fill='both')
+        self.systems_tree.bind('<<TreeviewSelect>>',self.selectItemCallback)
+        self.pack(expand=True, fill='both',side=RIGHT)
+        self.combo["values"] = get_controlled_systems()
+        if self.combo["values"]:
+          self.combo.current(0)
+          print(self.combo.get())
+          self.update_home_expansions(self.combo.get(),"Fathers of Nontime")
+        
+    def get_controlled_systems(self):
+      retun ["Naunin", "HR 6177", "Ratemere", "Maopi", "Smethells 1"]
+      
+    def overview_selection_changed(self,event):
+      system_name = self.combo.get()
+      self.update_home_expansions(system_name,"Fathers of Nontime")
+    
+    def update_home_expansions(self,home_system_name=None,controller_faction=None):
+      self.systems_tree.delete(*self.systems_tree.get_children())
+      i=1
+      if home_system_name:
+        for expansion_system in self.get_near_systems(home_system_name, controller_faction):
+          print(expansion_system)
+#         system_factions = star_system.get_current_factions()
+#         for faction in system_factions:
+#           f = bgs.Faction(faction)
+#           timestamp,influence = f.get_influence_in_system(system_name)[0]
+#           active_state = f.get_states('activeState')[0][2]
+#           pending_states = ", ".join([state[2] for state in f.get_states('pendingState')])
+#           recovering_states = ", ".join([state[2] for state in f.get_states('recoveringState')])
+          item = self.systems_tree.insert("", END, text=str(i), values=(expansion_system['name'],
+                                                                        expansion_system['distance'],
+                                                                        expansion_system['num_factions'],
+                                                                        expansion_system['has_player']))#, "YES" if f.is_player else "NO",""))
+          i+=1
+          
+    def get_near_systems(self,system_name,controller_faction=None):
+      result = []
+      star_system = bgs.System(system_name)
+      if star_system:
+        near_systems = star_system.get_closest_systems(10)
+        for near_sys in near_systems:
+          factions = bgs.System(near_sys["system"]).get_factions()
+          num_factions = len(factions)
+          has_player_faction = False
+          controlled = False
+          for faction in factions:
+            faction_obj = bgs.Faction(faction)
+            if faction_obj.name == controller_faction:
+              controlled = True
+            if faction_obj.is_player:
+              has_player_faction = True
+          result.append({"name":near_sys["system"],"distance":near_sys["distance"],"num_factions":num_factions,"controlled":controlled,"has_player":has_player_faction})
+      if result:
+        result = sorted(result,key=lambda x: float(x["distance"]))
+        print(result)
+      return result
       
     def selectItemCallback(self,event):
       pass
